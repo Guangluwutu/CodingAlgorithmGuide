280dfc209b82d1d397400944372b590eab7e0168
guangluwutu <reflexlyx@gmail.com>
2022-03-16 00:04:17 +0800

    update hanno

---
+++ CodingInterviewGuide/RC_DP/Solution.cpp
@@ -0,0 +1,24 @@
+
+#include <vector> 
+using namespace std;
+
+class Hanota {
+public:
+    void hanota(vector<int>& A, vector<int>& B, vector<int>& C) {
+        int n = A.size();
+        if (n>0){
+            move(n,A,B,C);
+        }
+    }
+    void move(int n,vector<int>& A,vector<int>& B,vector<int>& C){
+        if (n==1){
+            C.push_back(A.back());
+            A.pop_back();
+            return;
+        }
+        move(n-1,A,C,B);
+        C.push_back(A.back());
+        A.pop_back();
+        move(n-1,B,A,C);
+    }
+};
